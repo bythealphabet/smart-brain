56790895ebb2ae77baf40cbce8345b328b430f86
bythealphabet <bythealphabet@gmail.com>
2019-03-12 09:28:32 -0400

    Updates

---
+++ precache-manifest.37245cabb74080a8fe6411107af7a734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c087860485faf3239b573d8cfbcdc40",
+    "url": "/face/static/media/Brain-2-icon.9c087860.png"
+  },
+  {
+    "revision": "920179bd2c986d8ecdc3",
+    "url": "/face/static/js/runtime~main.920179bd.js"
+  },
+  {
+    "revision": "d08a40722f1e840a9267",
+    "url": "/face/static/js/main.d08a4072.chunk.js"
+  },
+  {
+    "revision": "14f80b9c705fc95a7871",
+    "url": "/face/static/js/1.14f80b9c.chunk.js"
+  },
+  {
+    "revision": "d08a40722f1e840a9267",
+    "url": "/face/static/css/main.bfec3606.chunk.css"
+  },
+  {
+    "revision": "14f80b9c705fc95a7871",
+    "url": "/face/static/css/1.79474aa3.chunk.css"
+  },
+  {
+    "revision": "da7502ee8bb72f247b066ab8f39529ac",
+    "url": "/face/index.html"
+  }
+];
